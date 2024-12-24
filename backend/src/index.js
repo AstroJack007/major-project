0029fb052ca944a8484fb8a6098a96a501486e20
Pranav <91676504+AstroJack007@users.noreply.github.com>
2024-12-24 17:53:26 +0530

    added routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,2 +1,16 @@
 const express=require('express');
 const app=express();
+
+const prisma=require('./db');
+
+const authRoutes=require('./routes/auth');
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(("/api/auth"),authRoutes);
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+}
+);
+
