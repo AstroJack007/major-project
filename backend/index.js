24a0a0b01f7407474a77bbff69ba69c2b1d0ba2c
Pranav <91676504+AstroJack007@users.noreply.github.com>
2024-12-28 02:02:00 +0530

    added routes and connected back and frontend

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ dotenv.config();
 const connectDB=require('./src/lib/db.js');
 const cookieparser=require("cookie-parser");
 const authRoutes=require('./src/routes/auth.js');
+const cors=require('cors');
 const msgRoutes=require('./src/routes/message.js');
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -14,6 +15,11 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieparser());
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true,
+}
+))
 app.use(("/api/auth"),authRoutes);
 app.use('/api/message',msgRoutes);
 
