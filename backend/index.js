3ff058451c3668b113ac3d141599747aa0d6549b
Pranav Kumar <91676504+AstroJack007@users.noreply.github.com>
2025-01-19 00:50:59 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,30 @@
 const express = require('express');
-const { app, server } = require('./src/lib/socket.js');
+const {app, server} = require('./src/lib/socket.js');
 const dotenv = require('dotenv');
-const { connectDB } = require('./src/lib/db.js');
+const {connectDB} = require('./src/lib/db.js');
 const cookieparser = require("cookie-parser");
 const authRoutes = require('./src/routes/auth.js');
 const cors = require('cors');
 const msgRoutes = require('./src/routes/message.js');
 const path = require("path");
 
-dotenv.config();
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieparser());
 app.use(cors({
-    origin: process.env.NODE_ENV === 'production' 
-        ? ['http://65.1.130.1', 'http://localhost:5173']
-        : 'http://localhost:5173',
+    origin: "http://localhost:5173",
     credentials: true,
 }));
 
 app.use("/api/auth", authRoutes);
 app.use('/api/message', msgRoutes);
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, "../frontend/my-project/dist")));
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(path.resolve(), "../frontend/my-project/dist")));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend/my-project/dist/index.html"));
+    app.get("*", (req,res) => {
+        res.sendFile(path.join(path.resolve(), "../frontend/my-project/dist/index.html"));
     });
 }
 
