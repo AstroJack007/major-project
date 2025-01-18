a5535eed06745d7899fb1bc4930e317adeeca04a
Pranav Kumar <91676504+AstroJack007@users.noreply.github.com>
2025-01-19 00:50:36 +0530

    Update auth.middleware.js

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,30 +1,38 @@
-const jwt = require('jsonwebtoken');
+const jwt =require('jsonwebtoken');
 const User = require('../models/user.js');
 
-const protectRoute = async (req, res, next) => {
-    try {
-        const token = req.cookies.jwt;
 
-        if (!token) {
-            return res.status(401).send("Unauthorized - No token provided");
+const protectRoute=async(req,res,next)=>{
+    try{
+        const token =req.cookies.jwt;
+
+        if(!token){
+            return res.status(401).send("Unauthorized- No token Provided");
+
         }
 
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        if (!decoded) {
-            return res.status(401).send("Unauthorized - Invalid token");
+        const decoded=jwt.verify(token,process.env.JWT_SECRET);
+        if(!decoded){
+            return res.status(401).send("Unauthorized- Invalid Provided");
         }
-
-        const user = await User.findById(decoded.userid).select('_id fullname email');
-        if (!user) {
+     
+        const user=await User.findById(decoded.userid).select('_id fullname email');
+        if(!user){
             return res.status(404).send("User not found");
         }
-        req.user = user;
-
+        req.user=user;
+        
         next();
-    } catch (err) {
-        console.log(err);
-        res.status(500).send("Internal Server Error");
+        }catch(err){
+            console.log(err);
+       
     }
+
+
+
+
+
+
 }
 
-module.exports = protectRoute;
+module.exports=protectRoute;
