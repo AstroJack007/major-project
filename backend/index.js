d2ac82807817874b8a1939e5fb45838cd145747a
Pranav Kumar <91676504+AstroJack007@users.noreply.github.com>
2025-01-18 23:22:00 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,38 +1,34 @@
 const express = require('express');
-const app = express();
-const {connectDB} = require('./backend/src/lib/db.js');
+const {app, server} = require('./src/lib/socket.js');
+const dotenv = require('dotenv');
+const {connectDB} = require('./src/lib/db.js');
 const cookieparser = require("cookie-parser");
-const authRoutes = require('./backend/src/routes/auth.js');
+const authRoutes = require('./src/routes/auth.js');
 const cors = require('cors');
-const msgRoutes = require('./backend/src/routes/message.js');
+const msgRoutes = require('./src/routes/message.js');
 const path = require("path");
 
 const port = process.env.PORT;
-const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieparser());
-app.use(
-  cors({
+app.use(cors({
     origin: "http://localhost:5173",
     credentials: true,
-  })
-);
+}));
 
 app.use("/api/auth", authRoutes);
 app.use('/api/message', msgRoutes);
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, "frontend/my-project/dist")));
-    
+    app.use(express.static(path.join(path.resolve(), "../frontend/my-project/dist")));
+
     app.get("*", (req,res) => {
-        res.sendFile(path.join(__dirname, "frontend/my-project/dist/index.html"));
+        res.sendFile(path.join(path.resolve(), "../frontend/my-project/dist/index.html"));
     });
 }
 
-connectDB().then(() => {
-    app.listen(port, () => {
-        console.log(`Server running on port ${port}`);
-    });
+server.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+    connectDB();
 });
-
-
