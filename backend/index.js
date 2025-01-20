59a682b153eb634627e702748d009956aa104edc
Pranav <91676504+AstroJack007@users.noreply.github.com>
2025-01-21 02:14:33 +0530

    ss

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 const path=require("path");
 
-const port = process.env.PORT || 3000; 
+const port = process.env.PORT || 4000; 
 
 
 app.use(express.json());
--- frontend/my-project/src/lib/axios.js
+++ frontend/my-project/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? 'http://localhost:3000/api':"http://52.66.244.205:3000/api",
+    baseURL: import.meta.env.MODE === "development" ? 'http://localhost:3000/api':"http://52.66.244.205:4000/api",
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
