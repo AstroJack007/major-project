296e4f576c0c11e6497cc94ad4c55d2f6f7ceb3e
Pranav Kumar <91676504+AstroJack007@users.noreply.github.com>
2025-01-20 18:58:02 +0530

    Update axios.js

--- frontend/my-project/src/lib/axios.js
+++ frontend/my-project/src/lib/axios.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? 'http://localhost:3000/api':"http://13.203.79.110",
+    baseURL: import.meta.env.MODE === "development" ? 'http://localhost:3000/api':"http://13.203.79.110/api",
     withCredentials: true,
 });
