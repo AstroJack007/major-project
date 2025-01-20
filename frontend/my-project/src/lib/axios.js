01e52ee0dd6fbaffbb2fa749e73fba265e3b2a39
Pranav <91676504+AstroJack007@users.noreply.github.com>
2025-01-20 20:13:21 +0530

    changes in axios

--- frontend/my-project/src/lib/axios.js
+++ frontend/my-project/src/lib/axios.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 
 export const axiosInstance = axios.create({
-    baseURL: "http://3.109.2.124/api",
+    baseURL: "http://3.109.2.124:3000/api",
     withCredentials: true,
 });
