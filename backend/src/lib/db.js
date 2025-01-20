56308717d4b5130995d1bd9b2a30e192257be427
Pranav Kumar <91676504+AstroJack007@users.noreply.github.com>
2025-01-20 19:08:26 +0530

    Update db.js

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URL);
-        console.log('MongoDB connected');
+        console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (err) {
         console.error(err.message);
         process.exit(1);
