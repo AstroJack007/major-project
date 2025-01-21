f3f03eda1e31c03e9e01048d737238cbff58021f
Pranav <91676504+AstroJack007@users.noreply.github.com>
2025-01-21 20:42:22 +0530

    cors change

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: ["http://52.66.244.205", "http://52.66.244.205:3000"],
+        origin: [ "http://localhost:5174"],
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
         credentials: true,
         allowedHeaders: ["Content-Type", "Authorization"],
